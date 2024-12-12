086e243b5e818e95225eef05d78707d7a3cc6e8e
Oliver Gaskell <og305@cam.ac.uk>
2024-12-12 19:15:41 +0000

    Remove ReplicaID - not necessary
    Signed-off-by: Oliver Gaskell <og305@cam.ac.uk>

--- CRFS/src/dummy_storage/storage.rs
+++ CRFS/src/dummy_storage/storage.rs
@@ -1,20 +1,22 @@
 use crate::errors;
-use crate::types::{ReplicaID, Hash, RawData};
+use crate::types::{Hash, RawData};
+
+use uuid::Uuid;
 
 enum Type { Binary, Integer, Float, String }
 
 // Look up where data is stored in a global data store, store/fetch under `id`
-fn store_obj(replica_id: ReplicaID, id: Hash, data: RawData) -> Result< (), errors::ErrorCode > {
+fn store_obj(replica_id: Uuid, id: Hash, data: RawData) -> Result< (), errors::ErrorCode > {
     return Err(errors::CODE_NOT_IMPL);
 }
-fn fetch_obj(replica_id: ReplicaID, id: Hash) -> Result< RawData, errors::ErrorCode > {
+fn fetch_obj(replica_id: Uuid, id: Hash) -> Result< RawData, errors::ErrorCode > {
     return Err(errors::CODE_NOT_IMPL);
 }
 
 // Metadata Store
-fn store_meta(replica_id: ReplicaID, key: String, data: &[u8], t: Type) -> Result< (), errors::ErrorCode > {
+fn store_meta(replica_id: Uuid, key: String, data: &[u8], t: Type) -> Result< (), errors::ErrorCode > {
     return Err(errors::CODE_NOT_IMPL);
 }
-fn fetch_meta(replica_id: ReplicaID, key: String) -> Result< (&'static [u8], Type), errors::ErrorCode > {
+fn fetch_meta(replica_id: Uuid, key: String) -> Result< (&'static [u8], Type), errors::ErrorCode > {
     return Err(errors::CODE_NOT_IMPL);
 }
--- CRFS/src/types.rs
+++ CRFS/src/types.rs
@@ -2,6 +2,5 @@ use sha2::Sha256;
 use uuid::Uuid;
 
 // Provisional Type Definitions
-pub type ReplicaID = Uuid;
 pub type Hash = Sha256;
 pub type RawData = Vec<u8>;
