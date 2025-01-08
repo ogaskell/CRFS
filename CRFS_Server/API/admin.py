125cd8a3988bc8d62692d3306dcc7146c9951f64
Oliver Gaskell <og305@cam.ac.uk>
2025-01-08 21:40:05 +0000

    Register models with admin.site

--- CRFS_Server/API/admin.py
+++ CRFS_Server/API/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import FileSystem, Replica, User
+
+admin.site.register(FileSystem)
+admin.site.register(Replica)
+admin.site.register(User)
