8bbe24068f193d4f682a37aa01b6b8df89afe588
Oliver Gaskell <og305@cam.ac.uk>
2025-03-18 14:55:02 +0000

    networking: add new API functions, update tests, more misc changes

--- CRFS/src/default_networking/networking.rs
+++ CRFS/src/default_networking/networking.rs
@@ -3,12 +3,49 @@ use crate::errors;
 use std::net;
 
 use json;
+use json::JsonValue;
+use rand::Rng;
 use regex::Regex;
 use reqwest;
 use serde::{Serialize, Deserialize};
 use uuid::Uuid;
 
-type VersionNumber = (u32, u32, u32);
+macro_rules! crfserror {
+    ($code: expr, $msg: expr) => {Err(Error::CRFSErr($code, $msg))};
+}
+
+#[derive(Debug, PartialEq)]
+pub struct VersionNumber(pub u32, pub u32, pub u32);
+pub const VERSION: VersionNumber = VersionNumber(0, 0, 1);
+
+impl VersionNumber {
+    pub fn from_str(version: String) -> Result<Self> {
+        let malformed: Result<VersionNumber> = crfserror!(errors::CODE_MALFORMED, format!("Version String {version} malformed."));
+
+        let re = Regex::new(r"(\d+)\.(\d+)(?:\.(\d+))?").unwrap();
+        let Some(cap) = re.captures(&version) else {
+            return malformed;
+        };
+
+        let ver: VersionNumber = VersionNumber(
+            match cap[1].parse() {Ok(v) => v, Err(_) => return malformed},
+            match cap[2].parse() {Ok(v) => v, Err(_) => return malformed},
+            match cap.get(3) {
+                Some(m) => match m.as_str().parse() {
+                    Ok(v) => v,
+                    Err(_) => return malformed,
+                },
+                None => 0u32,
+            },
+        );
+
+        return Ok(ver);
+    }
+
+    pub fn to_str(&self) -> String {
+        format!("{}.{}.{}", self.0, self.1, self.2)
+    }
+}
 
 #[derive(Debug)]
 pub enum Error {
@@ -19,20 +56,34 @@ pub enum Error {
 
 type Result<T> = std::result::Result<T, Error>;
 
-macro_rules! crfserror {
-    ($code: expr, $msg: expr) => {Err(Error::CRFSErr($code, $msg))};
-}
-
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct UserInfo {pub id: Option<Uuid>, pub disp_name: Option<String>}
 
-#[derive(Serialize, Deserialize)]
+impl UserInfo {
+    pub fn clone(&self) -> Self {
+        return Self { id: self.id.clone(), disp_name: self.disp_name.clone() }
+    }
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct FileSystemInfo {pub user: UserInfo, pub id: Option<Uuid>, pub disp_name: Option<String>}
 
-#[derive(Serialize, Deserialize)]
+impl FileSystemInfo {
+    pub fn clone(&self) -> Self {
+        return Self { user: self.user.clone(), id: self.id.clone(), disp_name: self.disp_name.clone() }
+    }
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct ReplicaInfo {pub fs: FileSystemInfo, pub id: Option<Uuid>, pub disp_name: Option<String>}
 
-#[derive(Serialize, Deserialize)]
+impl ReplicaInfo {
+    pub fn clone(&self) -> Self {
+        return Self { fs: self.fs.clone(), id: self.id.clone(), disp_name: self.disp_name.clone() }
+    }
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Status {
     pub server: Option< net::SocketAddr >,
     pub info: ReplicaInfo,
@@ -56,8 +107,43 @@ impl Status {
             pull_changes: false, push_changes: false,
         }
     }
+
+    pub fn clone(&self) -> Self {
+        Status {
+            server: self.server.clone(),
+            info: self.info.clone(),
+            ready: self.ready, data_ready: self.data_ready,
+            pull_changes: self.pull_changes, push_changes: self.push_changes,
+        }
+    }
 }
 
+pub struct Message {
+    transaction_id: Option<i64>,
+    message_type: String,
+    payload: json::JsonValue,
+}
+
+impl Message {
+    pub fn to_json(&self) -> JsonValue {
+        let mut rng = rand::rng();
+
+        let mut root = JsonValue::new_object();
+
+        root["version"] = JsonValue::from(VERSION.to_str());
+        root["transaction_id"] = JsonValue::from(match self.transaction_id {
+            Some(v) => v,
+            None => rng.random::<i64>(),
+        });
+        root["reply"] = JsonValue::from(false);
+        root["type"] = JsonValue::from(self.message_type.clone());
+        root["payload"] = JsonValue::from(self.payload.clone());
+
+        return root;
+    }
+}
+
+#[derive(Debug)]
 pub struct Response {
     pub version: VersionNumber,
     pub transaction_id: i64,
@@ -67,7 +153,7 @@ pub struct Response {
     pub notifications: json::Array,
 }
 
-pub fn send_json_message(status: Status, body: json::JsonValue) -> Result<(u16, json::JsonValue)> {
+pub fn send_json_message(status: Status, body: JsonValue) -> Result<(u16, JsonValue)> {
     let host = status.server.unwrap();
 
     let body = json::stringify(body);
@@ -89,36 +175,15 @@ pub fn send_json_message(status: Status, body: json::JsonValue) -> Result<(u16, 
     return Ok((status, res_body));
 }
 
-pub fn str_to_ver(version: String) -> Result<VersionNumber> {
-    let malformed: Result<VersionNumber> = crfserror!(errors::CODE_MALFORMED, format!("Version String {version} malformed."));
-
-    let re = Regex::new(r"(\d+)\.(\d+)(?:\.(\d+))?").unwrap();
-    let Some(cap) = re.captures(&version) else {
-        return malformed;
-    };
-
-    let ver: VersionNumber = (
-        match cap[1].parse() {Ok(v) => v, Err(_) => return malformed},
-        match cap[2].parse() {Ok(v) => v, Err(_) => return malformed},
-        match cap.get(3) {
-            Some(m) => match m.as_str().parse() {
-                Ok(v) => v,
-                Err(_) => return malformed,
-            },
-            None => 0u32,
-        },
-    );
-
-    return Ok(ver);
-}
-
-pub fn res_body_to_response(res_body: json::JsonValue) -> Result<Response> {
+pub fn res_body_to_response(res_body: JsonValue) -> Result<Response> {
     let malformed: Result<Response> = crfserror!(errors::CODE_MALFORMED, format!("Reponse malformed."));
 
-    let ver = str_to_ver(
+    println!("{:?}", res_body);
+
+    let ver = VersionNumber::from_str(
         String::from(res_body["version"].as_str().unwrap())
     )?;
-    let Ok(tid) = res_body["transaction_id"].as_str().unwrap().parse() else {return malformed};
+    let Some(tid) = res_body["transaction_id"].as_i64() else {return malformed};
     let Some(reply) = res_body["reply"].as_bool() else {return malformed};
 
     let message_type = match res_body["message_type"].as_str() {
@@ -126,11 +191,11 @@ pub fn res_body_to_response(res_body: json::JsonValue) -> Result<Response> {
         None => return malformed,
     };
     let payload = match res_body["payload"].clone() {
-        json::JsonValue::Object(o) => o,
+        JsonValue::Object(o) => o,
         _ => return malformed,
     };
     let notifications = match res_body["notifications"].clone() {
-        json::JsonValue::Array(a) => a,
+        JsonValue::Array(a) => a,
         _ => return malformed,
     };
 
@@ -142,4 +207,68 @@ pub fn res_body_to_response(res_body: json::JsonValue) -> Result<Response> {
         payload: payload,
         notifications: notifications,
     })
+}
+
+pub fn ping_server(status: Status) -> Result<Response> {
+    let message = Message {
+        transaction_id: None,
+        message_type: String::from("ping"),
+        payload: JsonValue::new_object(),
+    }.to_json();
+
+    let (code, res_) = send_json_message(status, message)?;
+    let res = res_body_to_response(res_)?;
+
+    if code == 200 {return Ok(res)} else {
+        return crfserror!(errors::CODE_ERROR, format!("Got code {code} from ping. Reply was:\n\t{:?}", res));
+    };
+}
+
+pub fn check_user(status: Status) -> Result<Response> {
+    let mut message = Message {
+        transaction_id: None,
+        message_type: String::from("check_user"),
+        payload: JsonValue::new_object(),
+    };
+
+    let mut encode_buf = Uuid::encode_buffer();
+    let uuid_str = status.info.fs.user.id.clone().unwrap().simple().encode_lower(&mut encode_buf);
+
+    message.payload["user_uuid"] = JsonValue::from(String::from(uuid_str));
+    let json_msg = message.to_json();
+
+    let (code, res_) = send_json_message(status, json_msg)?;
+    let res = res_body_to_response(res_)?;
+
+    let error_code = res.payload["code"].as_u32().unwrap();
+
+    if error_code > 0 {return crfserror!(error_code, String::from(""))}
+    else {return Ok(res)}
+}
+
+pub fn register_user(status: &mut Status) -> Result<Response> {
+    let mut message = Message {
+        transaction_id: None,
+        message_type: String::from("register_user"),
+        payload: JsonValue::new_object(),
+    };
+
+    let mut encode_buf = Uuid::encode_buffer();
+    let uuid_str = status.info.fs.user.id.clone().unwrap().simple().encode_lower(&mut encode_buf);
+
+    message.payload["user_uuid"] = JsonValue::from(String::from(uuid_str));
+    match status.info.fs.user.disp_name.clone() {
+        Some(n) => {message.payload["display_name"] = JsonValue::from(n);},
+        None => {},
+    }
+
+    let json_msg = message.to_json();
+
+    let (code, res_) = send_json_message(status.clone(), json_msg)?;
+    let res = res_body_to_response(res_)?;
+
+    let error_code = res.payload["code"].as_u32().unwrap();
+
+    if error_code > 0 {return crfserror!(error_code, String::from(""))}
+    else {return Ok(res)}
 }
--- CRFS/src/tests/networking_test.rs
+++ CRFS/src/tests/networking_test.rs
@@ -3,6 +3,7 @@ use crate::default_networking::networking;
 use std::net::{Ipv4Addr, SocketAddrV4};
 
 use json;
+use uuid::{uuid, Uuid};
 
 #[test]
 pub fn check_user_test() {
@@ -10,33 +11,31 @@ pub fn check_user_test() {
 
     let mut status = networking::Status::empty();
     status.server = Some(std::net::SocketAddr::V4(socket));
+    status.info.fs.user.id = Some(uuid!("ad0ff637-87a8-4c64-a1a0-2ed08ec15e66"));
 
-    let body = json::parse(r#"
+//     let body = json::parse(r#"
 
-{
-    "type": "check_user",
-    "transaction_id": "0",
-    "payload": {
-        "user_uuid": "ad0ff637-87a8-4c64-a1a0-2ed08ec15e66"
-    }
-}
+// {
+//     "type": "check_user",
+//     "transaction_id": 0,
+//     "payload": {
+//         "user_uuid": "ad0ff637-87a8-4c64-a1a0-2ed08ec15e66"
+//     }
+// }
 
-"#).unwrap();
+// "#).unwrap();
 
-    let (code, body) = match networking::send_json_message(status, body) {
-        Ok(v) => v,
-        Err(networking::Error::CRFSErr(code, msg)) => {panic!("CRFS Error: Code {}, Message:\n\t{}\n", code, msg);},
-        Err(networking::Error::JsonErr(e)) => {panic!("JSON Error: \n\t{:?}\n", e);},
-        Err(networking::Error::ReqwestErr(e)) => {panic!("Reqwest Error: \n\t{:?}\nDid you forget to start the server?\n", e);},
-    };
-    // println!("Code {}, body:\n{}\n> end body", code, body.to_string());
+//     let (code, body) = match networking::send_json_message(status, body) {
+//         Ok(v) => v,
+//         Err(networking::Error::CRFSErr(code, msg)) => {panic!("CRFS Error: Code {}, Message:\n\t{}\n", code, msg);},
+//         Err(networking::Error::JsonErr(e)) => {panic!("JSON Error: \n\t{:?}\n", e);},
+//         Err(networking::Error::ReqwestErr(e)) => {panic!("Reqwest Error: \n\t{:?}\nDid you forget to start the server?\n", e);},
+//     };
+//     // println!("Code {}, body:\n{}\n> end body", code, body.to_string());
 
-    assert_eq!(code, 200u16);
+    let response = networking::check_user(status).unwrap();
 
-    let response = networking::res_body_to_response(body).unwrap();
-
-    assert_eq!(response.version, (1u32, 0u32, 0u32));
-    assert_eq!(response.transaction_id, 0i64);
+    assert_eq!(response.version, networking::VERSION);
     assert_eq!(response.reply, true);
     assert_eq!(response.message_type, "check_user");
 
@@ -45,4 +44,14 @@ pub fn check_user_test() {
         json::stringify_pretty(response.payload, 2),
         json::stringify_pretty(response.notifications, 2)
     );
+}
+
+#[test]
+pub fn test_ping() {
+    let socket = SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), 8000);
+
+    let mut status = networking::Status::empty();
+    status.server = Some(std::net::SocketAddr::V4(socket));
+
+    networking::ping_server(status).unwrap();
 }
