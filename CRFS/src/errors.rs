5a4d928264b0fc1bedee12f62e78b188e7042410
Oliver Gaskell <og305@cam.ac.uk>
2025-03-18 14:53:05 +0000

    errors: add new Error struct, and client-specific error codes

--- CRFS/src/errors.rs
+++ CRFS/src/errors.rs
@@ -1,5 +1,21 @@
+use crate::default_networking::networking;
+
 pub type ErrorCode = u32;
 
+#[derive(Debug)]
+pub struct Error(pub ErrorCode, pub String);
+
+impl From<networking::Error> for Error {
+    fn from(e: networking::Error) -> Self {
+        match e {
+            networking::Error::CRFSErr(code, msg) => Self(code, msg),
+            networking::Error::JsonErr(e) => Self(CODE_JSON_ERR, format!("JSON Error: {:#?}", e)),
+            networking::Error::ReqwestErr(e) => Self(CODE_NET_ERR, format!("Reqwest Error: {:#?}", e)),
+        }
+    }
+}
+
+// Errors in the range 0x0000---- are networking protocol errors
 pub const CODE_SUCCESS: ErrorCode = 0;
 pub const CODE_ERROR: ErrorCode = 1;
 pub const CODE_COLLISION: ErrorCode = 2;
@@ -10,3 +26,7 @@ pub const CODE_NOT_FOUND: ErrorCode = 6;
 pub const CODE_NOT_IMPL: ErrorCode = 7;
 pub const CODE_MALFORMED: ErrorCode = 8;
 pub const CODE_AUTH_ERR: ErrorCode = 9;
+
+// Errors in the range 0x0001---- are specific to this Client
+pub const CODE_JSON_ERR: ErrorCode = 0x00010001;
+pub const CODE_NET_ERR: ErrorCode = 0x00010002;
