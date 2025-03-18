f5aa7fcb77ef136abb9e107f07fb792d86142667
Oliver Gaskell <og305@cam.ac.uk>
2025-03-18 14:53:22 +0000

    helpers: add get_named_arg

--- CRFS/src/helpers.rs
+++ CRFS/src/helpers.rs
@@ -18,3 +18,29 @@ pub fn ensure_dir(path: PathBuf) -> std::io::Result<bool> {
         Err(e) => Err(e),
     }
 }
+
+pub fn get_named_arg(args: &Vec<String>, short: Option<&str>, long: Option<&str>) -> Option<String> {
+    if short == None && long == None {panic!("Must provide short or long argument name.")}
+
+    let mut short_pre = String::from("-");
+    let mut long_pre = String::from("--");
+
+    let search_s = match short {
+        Some(s) => {short_pre.push_str(&s); args.iter().position(|x| *x == short_pre)},
+        None => None,
+    };
+
+    let search_l = match long {
+        Some(l) => {long_pre.push_str(&l); args.iter().position(|x| *x == long_pre)},
+        None => None,
+    };
+
+    let i = match (search_s, search_l) {
+        (Some(i), _) => i,
+        (None, Some(i)) => i,
+        (None, None) => return None,
+    };
+
+    let s = args.get(i + 1)?;
+    return Some(s.clone());
+}
