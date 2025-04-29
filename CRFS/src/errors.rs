5c574678d2fe26ac946e105563e00db1dfb26117
Oliver Gaskell <og305@cam.ac.uk>
2025-04-29 11:46:26 +0100

    errors: add conversion from io::Error and unit

--- CRFS/src/errors.rs
+++ CRFS/src/errors.rs
@@ -15,6 +15,18 @@ impl From<networking::Error> for Error {
     }
 }
 
+impl From<std::io::Error> for Error {
+    fn from(e: std::io::Error) -> Self {
+        Self(CODE_IO_ERR, e.to_string())
+    }
+}
+
+impl From<()> for Error {
+    fn from(e: ()) -> Self {
+        Self(CODE_ERROR, String::from("Unknown error."))
+    }
+}
+
 // Errors in the range 0x0000---- are networking protocol errors
 pub const CODE_SUCCESS: ErrorCode = 0;
 pub const CODE_ERROR: ErrorCode = 1;
@@ -30,3 +42,4 @@ pub const CODE_AUTH_ERR: ErrorCode = 9;
 // Errors in the range 0x0001---- are specific to this Client
 pub const CODE_JSON_ERR: ErrorCode = 0x00010001;
 pub const CODE_NET_ERR: ErrorCode = 0x00010002;
+pub const CODE_IO_ERR: ErrorCode = 0x00010003;
