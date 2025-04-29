10711b3f0bd1ce768cda39af3b563df3062c67cf
Oliver Gaskell <og305@cam.ac.uk>
2025-04-29 12:39:32 +0100

    helpers: Add LCS algorithm

--- CRFS/src/helpers.rs
+++ CRFS/src/helpers.rs
@@ -44,3 +44,17 @@ pub fn get_named_arg(args: &Vec<String>, short: Option<&str>, long: Option<&str>
     let s = args.get(i + 1)?;
     return Some(s.clone());
 }
+
+pub fn lcs<T>(x: &[T], y: &[T]) -> Vec<T> where T: Eq + Clone {
+    if x.len() == 0 || y.len() == 0 {return Vec::new();}
+    if x[x.len() - 1] == y[y.len() - 1] {
+        let mut result = Vec::from(lcs(&x[0..x.len() - 1], &y[0..y.len() - 1]));
+        result.push(x[x.len() - 1].clone());
+        return result;
+    }
+
+    let a = lcs(&x[0..x.len()], &y[0..y.len() - 1]);
+    let b = lcs(&x[0..x.len() - 1], &y[0..y.len()]);
+
+    if a.len() > b.len() {return a;} else {return b;}
+}
