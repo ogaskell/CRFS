fe45fc301b9d466508c86c0073038388f341eff6
Oliver Gaskell <og305@cam.ac.uk>
2025-05-17 03:17:04 +0100

    complete core, and main program

--- CRFS/src/core.rs
+++ CRFS/src/core.rs
@@ -16,8 +16,15 @@ pub struct SystemConfig(pub storage::Config, pub networking::Config);
 impl SystemConfig {
     pub fn get_replica_id(&self) -> Option<Uuid> { self.1.info.get_replica_id() }
 
+    pub fn init(&self) -> Result<(), errors::Error> {
+        let tree = file_tree::FileManager::read_or_init(&self.0, self.get_replica_id().unwrap())?;
+        tree.write_out()?;
+
+        Ok(())
+    }
+
     pub fn sync(&self) -> Result<(), errors::Error> {
-        let mut tree = file_tree::FileManager::read_or_init(&self.0, self.1.info.get_replica_id().unwrap())?;
+        let mut tree = file_tree::FileManager::read_or_init(&self.0, self.get_replica_id().unwrap())?;
 
         println!("-> File Tree loaded. Checking for local updates...");
 
@@ -28,7 +35,7 @@ impl SystemConfig {
         let remote_hashes = self.network_sync(&tree)?;
 
         if remote_hashes.len() > 0 {
-            println!("Applying {} ops...", remote_hashes.len());
+            // println!("Applying {} ops...", remote_hashes.len());
             tree.apply_ops(&remote_hashes.iter().collect())?;
         }
 
@@ -82,11 +89,79 @@ impl GlobalConfig {
         }
     }
 
-    pub fn write_out(&self) -> std::io::Result<()> {
-        storage::meta::write_at(&storage::Config{working_dir: PathBuf::new(), }, &Self::get_conf_path(), false, self)
+    pub fn write_out(&self, path: &PathBuf) -> std::io::Result<()> {
+        storage::meta::write_at(&storage::Config{working_dir: PathBuf::new(), }, path, false, self)
     }
 
-    pub fn read() -> std::io::Result<Self> {
-        storage::meta::read_at(&storage::Config{working_dir: PathBuf::new(), }, &Self::get_conf_path(), false)
+    pub fn read(path: &PathBuf) -> std::io::Result<Self> {
+        storage::meta::read_at(&storage::Config{working_dir: PathBuf::new(), }, path, false)
     }
+}
+
+pub fn init(path: &PathBuf) -> std::io::Result<()> {
+    if !path.exists() {
+        println!("Creating global config file...");
+        return GlobalConfig::empty().write_out(path);
+    } else {
+        println!("Global config exists.");
+        return Ok(());
+    }
+}
+
+pub fn setup(conf: &mut GlobalConfig, conf_path: &PathBuf, server: &std::net::SocketAddr, user_id: &Option<Uuid>, fs_id: &Option<Uuid>, user_name: &Option<String>, fs_name: &Option<String>, dir: &Option<PathBuf>) {
+    let working_dir = match dir {
+        Some(d) => d.clone(),
+        None => std::env::current_dir().expect("Error opening working directory. Move to a different directory, or specify a working directory."),
+    };
+
+    let mut system_config = SystemConfig(
+        storage::Config {working_dir}, networking::Config {
+            server: Some(server.clone()),
+            info: networking::ReplicaInfo {
+                id: user_id.clone(),
+                disp_name: None,
+                fs: networking::FileSystemInfo {
+                    id: fs_id.clone(),
+                    disp_name: fs_name.clone(),
+                    user: networking::UserInfo {
+                        id: user_id.clone(),
+                        disp_name: user_name.clone(),
+                    }
+                }
+            }
+        }
+    );
+    system_config.1.gen_blanks();
+
+    let (user_ok, fs_ok) = system_config.1.check_info().expect("Error checking info with server.");
+
+    if !user_ok {
+        system_config.1.register_user().expect("Error registering user.");
+    }
+
+    if !fs_ok {
+        system_config.1.register_fs().expect("Error registering FS");
+    }
+
+    println!("Identity confirmed with server.");
+
+    system_config.init().expect("Error setting up replica config.");
+
+    conf.replicas.push(system_config);
+    conf.write_out(conf_path).expect("Error writing global config.");
+
+    println!("Done!");
+}
+
+pub fn sync(conf: GlobalConfig, dir_: &Option<PathBuf>) {
+    let dir = match dir_ {
+        Some(d) => d.clone(),
+        None => std::env::current_dir().expect("Error opening working directory. Move to a different directory, or specify a working directory."),
+    };
+
+    let system_config = conf.find_replica_by_dir(dir).expect("Replica not found. Please run the setup command first.");
+
+    system_config.sync().expect("Sync error.");
+
+    println!("Sync OK!");
 }
--- CRFS/src/main.rs
+++ CRFS/src/main.rs
@@ -1,235 +1,82 @@
 mod default_storage;
-use crate::default_storage::storage;
-use crate::default_storage::outputs;
-
-mod default_networking;
-use crate::default_networking::networking;
-use crate::default_networking::networking::{UserInfo, FileSystemInfo, ReplicaInfo};
-
+mod networking;
 mod conflict_res;
-
-#[cfg(test)]
-mod tests;
-
+mod core;
 mod errors;
 mod helpers;
 mod types;
 
-use std::env;
-use std::net::{Ipv4Addr, SocketAddr, SocketAddrV4};
+#[cfg(test)]
+mod tests;
+
+use default_storage::storage;
+
 use std::path::PathBuf;
 
-use homedir::my_home;
-use serde::{Serialize, Deserialize};
+use clap::{Parser, Subcommand};
 use uuid::Uuid;
 
-type SystemConfig = (storage::Config, networking::Config);
+#[derive(Parser)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
 
-#[derive(Serialize, Deserialize, Debug, Clone)]
-struct GlobalConfig {
-    pub replicas: Vec<SystemConfig>,
+    /// Location of the global config file. Defaults to ~/.config/crfs/config.json
+    #[arg(short)]
+    global_conf: Option<PathBuf>,
 }
 
-impl GlobalConfig {
-    pub fn clone(&self) -> Self {
-        Self {
-            replicas: self.replicas.clone(),
-        }
-    }
-
-    pub fn find_replica_by_dir(&self, dir: PathBuf) -> Option<SystemConfig> {
-        self.replicas.clone().into_iter().find(|x| *x.0.working_dir == dir)
-    }
-
-    pub fn replica_index_by_dir(&self, dir: PathBuf) -> Option<usize> {
-        self.replicas.clone().into_iter().position(|x| *x.0.working_dir == dir)
-    }
-
-    pub fn empty() -> Self {
-        Self {
-            replicas: Vec::new(),
-        }
+#[derive(Subcommand, PartialEq, Eq)]
+enum Commands {
+    /// Create global config.
+    Init,
+    /// Create and set up a replica.
+    Setup {
+        /// Hostname of the remote server to use.
+        #[arg(short)]
+        server: std::net::SocketAddr,
+        /// User UUID. If omitted, a new user will be created.
+        #[arg(short)]
+        user_id: Option<Uuid>,
+        /// Filesystem UUID. If omitted, a new FS will be created.
+        #[arg(short)]
+        fs_id: Option<Uuid>,
+        /// User's name. Optional
+        /// If the user exists, this will overwrite any existing name.
+        #[arg(long)]
+        user_name: Option<String>,
+        /// Name for the filesystem. Optional
+        /// If the filesystem exists, this will overwrite any existing name.
+        #[arg(long)]
+        fs_name: Option<String>,
+        /// Replica directory. Defaults to the current directory.
+        #[arg(short)]
+        dir: Option<PathBuf>,
+    },
+    /// Synchronise a replica with the server.
+    Sync {
+        /// Replica directory. Defaults to the current directory.
+        dir: Option<PathBuf>
     }
 }
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let cli = Cli::parse();
 
-    let mut networkconf = networking::Config::empty();
+    let conf_path = match cli.global_conf {
+        None => core::GlobalConfig::get_conf_path(),
+        Some(p) => p,
+    };
 
-    let mut globalconfpath = my_home().unwrap().unwrap();
-    globalconfpath.push(storage::GLOBALCONF);
+    if cli.command == Commands::Init {return core::init(&conf_path).expect("Initialisation Error.");}
 
-    match args[1].as_str() {
-        "setup" => {
-            // CLI Args
-            let addr_i = args.iter().position(|x| x == "-s" || x == "--server");
-            let address = match addr_i {
-                Some(i) => args[i + 1].parse().unwrap(),
-                None => SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), 8000),
-            };
-            networkconf.server = Some(SocketAddr::V4(address));
+    let mut conf = core::GlobalConfig::read(&conf_path).expect("Error reading global config. Please run the init command first.");
 
-            networkconf.info.fs.user.id = match helpers::get_named_arg(&args, Some("u"), Some("user-uuid")) {
-                Some(u) => Some(Uuid::parse_str(u.as_str()).unwrap()),
-                None => None,
-            };
-            networkconf.info.fs.user.disp_name = helpers::get_named_arg(&args, None, Some("user-name"));
-            networkconf.info.fs.id = match helpers::get_named_arg(&args, Some("f"), Some("fs-uuid")) {
-                Some(f) => Some(Uuid::parse_str(f.as_str()).unwrap()),
-                None => None,
-            };
-            networkconf.info.fs.disp_name = helpers::get_named_arg(&args, None, Some("fs-name"));
-            networkconf.info.id = match helpers::get_named_arg(&args, Some("r"), Some("replica-uuid")) {
-                Some(r) => Some(Uuid::parse_str(r.as_str()).unwrap()),
-                None => None,
-            };
-            networkconf.info.disp_name = helpers::get_named_arg(&args, None, Some("replica-name"));
-
-            let mut workingdir = helpers::get_named_arg(&args, Some("d"), Some("dir"));
-            let workingdir_canonical = match &workingdir {
-                Some(s) => PathBuf::from(s).canonicalize(),
-                None => {workingdir = Some(String::from(".")); PathBuf::from(".").canonicalize()},
-            };
-
-            // Setup replica
-            setup_replica(&mut networkconf).unwrap();
-
-            // dbg!(netconfig.clone());
-
-            println!(
-                "Network Setup Done!\n\tUser: {:#?}\n\tFS: {:#?}\n\tReplica: {:#?}",
-                networkconf.info.fs.user.id,
-                networkconf.info.fs.id,
-                networkconf.info.id
-            );
-
-            let storageconf = storage::Config {
-                working_dir: match workingdir_canonical {
-                    Ok(path) => path,
-                    Err(_) => {println!("Malformed directory: {}", workingdir.unwrap()); return;}
-                }
-            };
-
-            println!("Setting up local storage in {:?}", storageconf.working_dir);
-
-            let concreplicainfo = outputs::ConcreteReplicaInfo::from_replicainfo(networkconf.info.clone()).unwrap();
-            let mut infofile = storage::MetaFile::<outputs::ConcreteReplicaInfo>::create_at_path(
-                PathBuf::from("replica.crfs")
-            ).unwrap();
-            infofile.write(&concreplicainfo).unwrap();
-
-            add_replica_to_conf(globalconfpath, (storageconf, networkconf));
-
-            println!("Setup Complete! replica.crfs contains details of your account, filesystem and replica.");
+    match &cli.command {
+        Commands::Setup {server, user_id, fs_id, user_name, fs_name, dir} => {
+            core::setup(&mut conf, &conf_path, server, user_id, fs_id, user_name, fs_name, dir);
         },
-        "check" => {
-            let mut conffile = match storage::MetaFile::<GlobalConfig>::open_path(globalconfpath) {
-                Ok(c) => c,
-                Err(e) => {
-                    println!("Failed to open global config file! Error: {:#?}", e);
-                    return
-                }
-            };
-            let conf = conffile.read().unwrap();
-
-            let mut workingdir = helpers::get_named_arg(&args, Some("d"), Some("dir"));
-            let workingdir_canonical = match &workingdir {
-                Some(s) => PathBuf::from(s).canonicalize(),
-                None => {workingdir = Some(String::from(".")); PathBuf::from(".").canonicalize()},
-            }.unwrap();
-
-            let replica = match conf.find_replica_by_dir(workingdir_canonical.clone()) {
-                Some(config) => {
-                    println!("Found config for replica at {:?}", config.clone().0.working_dir);
-                    config
-                },
-                None => {
-                    println!("Failed to find replica at {:?}", workingdir_canonical);
-                    return;
-                },
-            };
-        },
-        "remove" => {
-            let mut conf_read_file = match storage::MetaFile::<GlobalConfig>::open_path(globalconfpath.clone()) {
-                Ok(c) => c,
-                Err(e) => {
-                    println!("Failed to open global config file! Error: {:#?}", e);
-                    return
-                },
-            };
-            let mut conf = conf_read_file.read().unwrap();
-
-            let workingdir = helpers::get_named_arg(&args, Some("d"), Some("dir"));
-            let workingdir_canonical = match &workingdir {
-                Some(s) => PathBuf::from(s).canonicalize(),
-                None => PathBuf::from(".").canonicalize(),
-            }.unwrap();
-
-            let replica_ind = match conf.replica_index_by_dir(workingdir_canonical.clone()) {
-                Some(index) => index,
-                None => {
-                    println!("Failed to find replica at {:?}", workingdir_canonical);
-                    return;
-                },
-            };
-
-            conf.replicas.remove(replica_ind);
-            let mut conf_write_file = storage::MetaFile::<GlobalConfig>::create_at_path(globalconfpath).unwrap();
-            conf_write_file.write(&conf).unwrap();
-
-            println!("Removed replica at {:?}", workingdir_canonical);
-            println!("{:?} directory has been left intact, but this can now be safely removed.", workingdir_canonical.join(PathBuf::from(".crfs")))
-        },
-        _ => {
-            println!("Unknown command!");
-            return;
-        },
+        Commands::Sync {dir} => core::sync(conf, dir),
+        _ => {panic!();}
     }
-}
-
-fn setup_replica(config: &mut networking::Config) -> Result<(), errors::Error> {
-    setup_fs(config)?;
-
-    config.info.id = match config.info.id {
-        Some(v) => Some(v),
-        None => Some(Uuid::now_v7()),
-    };
-
-    return Ok(());
-}
-
-fn setup_fs(config: &mut networking::Config) -> Result<(), errors::Error> {
-    setup_user(config)?;
-
-    config.info.fs.id = match config.info.fs.id {
-        Some(v) => Some(v),
-        None => Some(Uuid::now_v7()),
-    };
-
-    return Ok(());
-}
-
-fn setup_user(config: &mut networking::Config) -> Result<(), errors::Error> {
-    config.info.fs.user.id = match config.info.fs.user.id {
-        Some(v) => Some(v),
-        None => Some(Uuid::now_v7()),
-    };
-    match networking::register_user(config) {
-        Ok(_) => Ok(()),
-        Err(neterr) => Err(errors::Error::from(neterr)),
-    }
-}
-
-fn add_replica_to_conf(loc: PathBuf, config: SystemConfig) -> () {
-    let read_file = storage::MetaFile::<GlobalConfig>::open_path(loc.clone());
-    let mut conf = match read_file {
-        Ok(mut f) => f.read().unwrap(),
-        Err(_) => GlobalConfig::empty(),
-    };
-
-    conf.replicas.push(config);
-
-    let mut write_file = storage::MetaFile::create_at_path(loc.clone()).unwrap();
-    write_file.write(&conf).unwrap();
 }
