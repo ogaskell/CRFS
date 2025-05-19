e0bb3eb16a3ff43f98783c6dab1278527991a055
Oliver Gaskell <og305@cam.ac.uk>
2025-05-19 04:17:16 +0100

    storage: removal of dead/unnecessary code

--- CRFS/src/storage/mod.rs
+++ CRFS/src/storage/mod.rs
@@ -1,6 +1,3 @@
-use crate::types::Hash;
-
-use std::option::Option;
 use std::path::PathBuf;
 
 use serde::{Serialize, Deserialize};
@@ -14,66 +11,5 @@ pub struct Config {
     pub working_dir: PathBuf,
 }
 
-impl Config {
-    pub fn clone(&self) -> Self {
-        Self {
-            working_dir: self.working_dir.clone(),
-        }
-    }
-}
-
 pub mod object;
 pub mod meta;
-
-// OBJECT FILE HANDLING
-#[derive(Clone, Debug, Serialize, Deserialize)]
-pub enum ObjectLocation {
-    ObjectStore(Config, Option<Hash>),
-    OnDisk(Config, PathBuf),
-}
-
-impl ObjectLocation {
-    pub fn get_path(&self) -> PathBuf {
-        match self {
-            ObjectLocation::OnDisk(c, p) => {
-                if p.is_relative() {
-                    let mut res = c.working_dir.clone(); res.push(p);
-                    res
-                } else {
-                    p.clone()
-                }
-            },
-            ObjectLocation::ObjectStore(c, h) =>
-                ObjectLocation::hash_to_path(c, &(h.unwrap())),
-        }
-    }
-
-    pub fn get_hash(&self) -> Option<&Hash> {
-        match self {
-            ObjectLocation::OnDisk(..) => None,
-            ObjectLocation::ObjectStore(_, hash) => match hash {
-                Some(h) => Some(h),
-                None => None,
-            },
-        }
-    }
-
-    pub fn hash_to_path(config: &Config, hash: &Hash) -> PathBuf {
-        let hex = format!("{:x}", hash);
-
-        let mut path = PathBuf::new();
-        path.push(&config.working_dir);
-        path.push(OBJECTDIR);
-        path.push(&hex[0..2]);
-        path.push(&hex[2..]);
-
-        return path;
-    }
-
-    pub fn extension(&self) -> Option<String> {
-        match self {
-            ObjectLocation::OnDisk(_, p) => Some(p.extension()?.to_owned().into_string().unwrap()),
-            ObjectLocation::ObjectStore(..) => None,
-        }
-    }
-}
